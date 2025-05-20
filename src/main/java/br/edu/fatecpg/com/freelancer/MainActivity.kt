c659b862cd14d769329ea8d621fcae25dd2bc9f3
cauecaramello <cauecaramello@hotmail.com>
2025-05-19 22:17:20 -0300

    backend 2

---
+++ src/main/java/br/edu/fatecpg/com/freelancer/MainActivity.kt
@@ -0,0 +1,41 @@
+package br.edu.fatecpg.com.freelancer
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        auth = Firebase.auth
+
+        auth.signInWithEmailAndPassword("matheus@gmail.com","1234567")
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "signInWithCustomToken:success")
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "signInWithCustomToken:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+
+    }
+}
